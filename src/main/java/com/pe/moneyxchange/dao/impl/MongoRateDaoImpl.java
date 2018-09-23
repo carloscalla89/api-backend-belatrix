ba5cfb6f96f78546d1863bfc801bf08da0363ff2
carlos calla <carlos.calla89@gmail.com>
2018-09-22 22:10:40 -0500

    testing service impl

--- src/main/java/com/pe/moneyxchange/dao/impl/MongoRateDaoImpl.java
+++ src/main/java/com/pe/moneyxchange/dao/impl/MongoRateDaoImpl.java
@@ -28,7 +28,7 @@ public class MongoRateDaoImpl implements RateDao<RateResponse> {
         Document document = mongoCollection.find(Filters.eq("rateType",typeRate)).first();
 
         return Optional.ofNullable(document).map(
-                doc -> Optional.ofNullable(doc.getDouble("rateValue2"))
+                doc -> Optional.ofNullable(doc.getDouble("rateValue"))
                         .map(r -> RateResponse.builder()
                                 .setRates(r)
                                 .setChangedAmount(r*amount)
